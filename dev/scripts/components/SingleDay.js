8a26ca19f24eb7e2cca530575d06ab3eff9c54bc
Meghan Sterling <sterling.meghan@gmail.com>
2018-06-21 22:11:38 -0400

    added color change when all chores complete

--- dev/scripts/components/SingleDay.js
+++ dev/scripts/components/SingleDay.js
@@ -8,10 +8,12 @@ class SingleDay extends Component {
 
         this.state = {
             dayName: '',
-            choreList: []
+            choreList: [],
+            addCompletedClass: false
         }
 
         this.finishedChore = this.finishedChore.bind(this);
+        this.finishedAllChores = this.finishedAllChores.bind(this);
     }
 
     componentDidMount() {
@@ -43,10 +45,13 @@ class SingleDay extends Component {
                         this.setState({
                             choreList: choreArray,
                             userID: user.uid
+                        }, () => {
+                            this.finishedAllChores();
                         });
                     });
             }
         });
+        
     }
 
     finishedChore(firebaseKey, isComplete) {
@@ -67,11 +72,30 @@ class SingleDay extends Component {
                 });
         }
     }
+
+    finishedAllChores() {
+        let allComplete = this.state.choreList.map((choreItem) => {
+             return choreItem.complete;
+        })
+        const addCompletedClass = allComplete.every((chore) => {
+            return chore === true;
+        })
+        if (addCompletedClass === true) {
+            this.setState ({
+                addCompletedClass: true
+            }); 
+        } else {
+            this.setState ({
+                addCompletedClass: false
+            });
+        }
+    }
     
 
     render() {
+        const completedClass = this.state.addCompletedClass === true ? "finished" : "";
         return (
-            <div className="card">
+            <div className={`card ${completedClass}`}>
                 <div className="card-header">{this.state.dayName}</div>
                 <ul className="list-group list-group-flush">
                 {this.state.choreList.map((choreItem) => {
